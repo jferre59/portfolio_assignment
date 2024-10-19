9ac1d15ab5a5a7470906be3b7e98b9e7a8097d0e
jferre59 <jferre59@my.centennialcollege.ca>
2024-10-19 19:44:36 -0400

    babel / config / connecting db

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.port || 4000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI || 
+    "mongodb+srv://jferre59:J1235813f!@cluster0.vt5as.mongodb.net/" ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_POST ||'27017') + 
+    '/Skeleton'
+};
+module.exports = config;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const cookieParser = require('cookie-parser');
+const compress = require('compression');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Welcome to DressStore Application.')
 })
 
-app.listen(3000)
+// config
+const config = require('./config/config.js');
+// mongoDB
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri,{useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+.then(()=>console.log("Database connected Sucessfully"))
+.catch(err => console.error(`Connection Failed:$(err.message)`));
+
+app.listen(config.port,()=>{
+  console.info(`Server started on the port ${config.port}`)
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+module.exports = app;
